9e0cf115ea651fb1ef16e6fee62a16bcff518d83
Ronnie Hedlund <huj56745@gmail.com>
2019-05-22 21:03:25 +0200

    add mouse smoothing (less choppy camera movement in non VR mode)

--- Assets/C#/CameraController.cs
+++ Assets/C#/CameraController.cs
@@ -13,6 +13,43 @@ public class CameraController : MonoBehaviour
     internal static bool bSnapMovement = false;
     internal static Vector3 vCamPos = new Vector3(0,0,0);
 
+    float fCurX = 0, fCurY = 0;
+    float fStepX = 0, fStepY = 0;
+    public void GetMouseMovementSmooth(out float o_fX, out float o_fY)
+    {
+        fCurX += Input.GetAxis("Mouse X");
+        fCurY += Input.GetAxis("Mouse Y");
+
+        float fStep = Time.deltaTime / 0.050f; //% of movement to distribute each time called
+        if (fStep > 1.0f) fStep = 1.0f;  //if frametime too long we must not move faster
+        if (fStep <= 0.0f) fStep = 1.0f; //if the timer has to low resolution compared to the framerate
+
+        fStepX = fCurX * fStep;
+        fStepY = fCurY * fStep;
+
+        if (Mathf.Abs(fCurX) > Mathf.Abs(fStepX))
+        {
+            fCurX -= fStepX;
+            o_fX = fStepX;
+        }
+        else
+        {
+            o_fX = fCurX;
+            fCurX = 0;
+        }
+
+        if (Mathf.Abs(fCurY) > Mathf.Abs(fStepY))
+        {
+            fCurY -= fStepY;
+            o_fY = fStepY;
+        }
+        else
+        {
+            o_fY = fCurY;
+            fCurY = 0;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,9 +66,13 @@ public class CameraController : MonoBehaviour
         //emulate headset movement
         if (Input.GetKey(KeyCode.F) || GameManager.bNoVR)
         {
-            if (Input.GetKey(KeyCode.G)) fZ += Input.GetAxisRaw("Mouse X") * 3.0f;
-            else fY += Input.GetAxisRaw("Mouse X") * 3.0f;
-            fX -= Input.GetAxisRaw("Mouse Y") * 3.0f;
+            //using mouse smoothing to avoid jerkyness
+            float fMouseX, fMouseY;
+            GetMouseMovementSmooth(out fMouseX, out fMouseY);
+            if (Input.GetKey(KeyCode.G)) fZ += fMouseX * 3.0f;
+            else fY += fMouseX * 3.0f;
+            fX -= fMouseY * 3.0f;
+
             if (Input.GetKey(KeyCode.R)) { fX = 5; fY = 0; fZ = 0; }
 
             transform.eulerAngles = new Vector3(fX, fY, fZ);
