38ee0a8fe008b9a548609f1ea5e2f2691d9f1370
Ronnie Hedlund <huj56745@gmail.com>
2021-04-10 17:07:18 +0200

    v1.66 gui fix

--- Assets/C#/GameManager.cs
+++ Assets/C#/GameManager.cs
@@ -736,6 +736,9 @@ public class GameManager : MonoBehaviour
                 break;
 
             case 8:
+                //moved here from state 10 as a bug fix
+                szLastLevel = GameLevel.szLevel;
+
                 //begin loading the level (or replay)
                 //Debug.Log("Load map Begin");
                 szToLoad = "Scenes/PlayGame";
@@ -822,7 +825,8 @@ public class GameManager : MonoBehaviour
                         if (!bNoHiscore && !bNoInternet && iLastLevelIndex<200)
                         {
                             //always update last level played
-                            szLastLevel = GameLevel.szLevel;
+                            //szLastLevel = GameLevel.szLevel;
+                            //^now done above at the time the play button is pressed
 
                             if (!GameLevel.bRunReplay && (GameLevel.theMap.player.bAchieveFinishedRaceLevel || GameLevel.theMap.bAchieveFinishedMissionLevel))
                             {
