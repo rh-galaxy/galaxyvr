327c3701d73143e4292c9e4bf2ee6b6658f1b15b
Ronnie Hedlund <huj56745@gmail.com>
2024-03-05 20:25:14 +0100

    change privacy.php

--- gfvr_web/privacy.php
+++ gfvr_web/privacy.php
@@ -26,18 +26,15 @@ headline("Galaxy Forces VR - Privacy Policy");
 <?php
 square_start("'100%'");
 ?>
-<p><i>Team Galaxy</i> is committed to protecting the privacy of players, although providing online hi-scores discloses your user name of the platform your game is playing on. This privacy policy describes how we use and protect any information that you give us. We may change this policy from time to time by updating this page.</p>
-
-<p>We may collect the following information:<br>
-<i>Your location (country).</i><br>
+<p><i>Team Galaxy</i> is committed to protecting the privacy of players.</p>
+<p>We collect and store the following information:<br>
 <i>The type of device you’re using.</i><br>
 <i>Your game play statistics - disclosed on this website.</i>
 </p>
-<p>We collect this information to provide you with a good achievement system, to keep you interested in the game.</p>
-<p>We are committed to ensuring that your information is secure. In order to prevent unauthorized access or disclosure we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.</p>
-<p>We will not send you an email if you don't send us one first. No other means of communication will be used. Mail to contact@galaxy-forces-vr.com.</p>
-<p>We will not sell, distribute or lease your personal information to third parties, unless we are required by law to do so.</p>
-<p>Under the data protection act 1998 you may request details of any personal information that we may have on you. If you believe that any information we are holding is incorrect or incomplete, please email us. We will correct any information found to be incorrect or delete such information.</p>
+<p>We collect this information to provide you with a good achievement system.</p>
+<p>We will not distribute your personal information to third parties, unless we are required by law to do so.</p>
+<p>You can request to delete this data by sending an email to contact@galaxy-forces-vr.com
+</p>
 
 
 <?php
