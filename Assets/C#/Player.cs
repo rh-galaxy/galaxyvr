4fc7ff7ae1a6f605ad60d3614faf9aaeb6cb4213
Ronnie Hedlund <huj56745@gmail.com>
2024-05-29 11:16:33 +0200

    add comment

--- Assets/C#/Player.cs
+++ Assets/C#/Player.cs
@@ -1052,6 +1052,7 @@ public class Player : MonoBehaviour
                 fDirection -= iNumCounts * 360.0f;
 
                 oRb.MoveRotation(fDirection);
+                //it's too late now but should have used oRb.angularVelocity...
             }
 
             //consume fuel
